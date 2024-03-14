64c366398debf45ebc87017a4468fc5213d1f2f2
timofey282228 <timofeyschoolmail@gmail.com>
2024-03-14 19:04:22 +0200

    benchmarking and stats

--- src/__main__.py
+++ src/__main__.py
@@ -1,10 +1,12 @@
 import argparse
+import time
 
 from miller_rabin import miller_rabin_test
 from trial_division import trial_division_pascal
 from pollard import pollard_factorization, pollard_floyd, floyd, classic, h
 from bm import brillhart_morrison
 from functools import partial
+from timestats import TimeStats
 
 
 def get_pollard_algo(s):
@@ -28,51 +30,72 @@ def canonical_add(canonical: dict[int, int], p: int):
 
 def canonical_to_str(canonical, latex=False):
     return (("$" if latex else "") +
-            " {} ".format("\\cdot" if latex else "*").join((str(pr) + "^" + str(po) for pr, po in canonical.items())) +
+            " {} ".format("\\cdot" if latex else "*").join(
+                (str(pr) + "^" + ("{" if latex else "") + str(po) + ("}" if latex else "") for pr, po in canonical.items())
+            ) +
             ("$" if latex else ""))
 
 
+def select_pollard(args):
+    # choosing Polard's rho method modification
+    if args.pollard_mod == "floyd_cycle":
+        return pollard_floyd
+    else:
+        return partial(pollard_factorization, f=lambda x: x ** 2 + 1, x0=1, method=get_pollard_algo(args.pollard_mod))
+
+
 def main(args):
+    if args.bench:
+        benchmark(args)
+    else:
+        factorize(args)
+
+
+def factorize(args):
     for n in args.n:
+        stats = TimeStats()
         current_n = n
         print(f"Factorizing n = {n}")
-        canonical: dict[int, int] = {}  # primes to corresponing powers in the canonnical representation
+        canonical: dict[int, int] = {}  # primes to corresponing powers in the canonical representation
         is_prime = False
 
         if miller_rabin_test(n):
             print("Was prime")
+            print(stats.checkpoint())
             is_prime = True
 
         while not is_prime and (d := trial_division_pascal(n)) is not None:
             print(f"Found {d} by trial division")
+            print(stats.checkpoint())
             canonical_add(canonical, d)
             n //= d
             is_prime = miller_rabin_test(n)
             if is_prime:
                 print(f"{n} is prime")
+                print(stats.checkpoint())
 
-        # choosing Polard's rho method modification
-        if args.pollard_mod == "floyd_cycle":
-            pollard = pollard_floyd
-        else:
-            pollard = partial(pollard_factorization, f=lambda x: x ** 2 + 1, x0=1, method=get_pollard_algo(args.pollard_mod))
+        pollard = select_pollard(args)
 
         # Only find one divisor
         if not is_prime and (d := pollard(n)) is not None:
             print(f"Found {d} by Pollard's rho method ({args.pollard_mod})")
+            print(stats.checkpoint())
             canonical_add(canonical, d)
             n //= d
             is_prime = miller_rabin_test(n, k=args.m)
             if is_prime:
                 print(f"{n} is prime")
+                print(stats.checkpoint())
 
         while not is_prime and (dd := brillhart_morrison(n, k=args.k, attempts=args.attempts)) is not None:
             print(f"Found {dd[0]} by Brillhart-Morrison's method")
+            print(stats.checkpoint())
             canonical_add(canonical, dd[0])
             n = int(n) // dd[0]
             is_prime = miller_rabin_test(n)
             if is_prime:
                 print(f"{n} is prime")
+                print(stats.checkpoint())
 
         if is_prime:
             if n != 1:
@@ -80,6 +103,52 @@ def main(args):
             print(f"Canonical representation of {current_n}: {canonical_to_str(canonical, latex=args.latex)}")
         else:
             print("Could not fully factorize")
+
+        print(stats.stop())
+        print()
+
+
+def benchmark(args):
+    algos = {
+        # "Pascal trial division": trial_division_pascal,
+        "Pollard's rho method with Floyd's cycle finding": pollard_floyd,
+        "Pollard's rho method (classic)": partial(pollard_factorization, f=lambda x: x ** 2 + 1, x0=1, method=classic),
+        "Pollard's rho method (floyd)": partial(pollard_factorization, f=lambda x: x ** 2 + 1, x0=1, method=floyd),
+        "Pollard's rho method (h)": partial(pollard_factorization, f=lambda x: x ** 2 + 1, x0=1, method=h),
+        "Brillhart-Morrison's method": lambda n: brillhart_morrison(n, k=args.k, attempts=args.attempts)[0]
+    }
+
+    for n in args.n:
+        if args.latex:
+            print(r"\begin{{table}}[h]\caption{{{}}}\begin{{tabular}}{{l|cr}}".format(n))
+        else:
+            print(f"Benchmarking on {n}")
+
+        header = ("Algorithm name", "Found divisor", "Execution time (µs)")
+        if args.latex:
+            print(
+                " & ".join(header) +  " \\\\"
+            )
+        else:
+            print(
+                "\t".join(header)
+            )
+
+        for algo_name, algo_func in algos.items():
+            t = time.perf_counter_ns()
+            d = algo_func(n)
+            t = (time.perf_counter_ns() - t) / 1000
+            row = (algo_name, str(d) if d is not None else "-", "{t:.1f}".format(t=t))
+            if args.latex:
+                print(
+                    " & ".join(row) + " \\\\"
+                )
+            else:
+                print(
+                    "\t".join(row)
+                )
+        if args.latex:
+            print(r"\end{tabular}\end{table}")
         print()
 
 
@@ -107,5 +176,13 @@ if __name__ == "__main__":
 
     display_options = argparser.add_argument_group(title="Display", description="Display options")
     display_options.add_argument("--latex", action='store_true', help="Output will be printed in latex where appropriate")
+
+    bench_options = argparser.add_argument_group(title="Benchmarking")
+    bench_options.add_argument(
+        "--bench",
+        action="store_true",
+        help="Run each available factorization algorithm once for each n, find one divisor and print stats"
+    )
+
     args = argparser.parse_args()
     main(args)
